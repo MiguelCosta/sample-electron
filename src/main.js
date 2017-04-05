57faaa3ad4045a956801ab5d0c33251ee60ff762
Miguel Costa <miguel.costa@inelcis.pt>
2017-04-05 14:58:56 +0100

    Refactor

--- src/main.js
+++ src/main.js
@@ -3,9 +3,16 @@ const electron = require('electron')
 const app = electron.app
 const BrowserWindows = electron.BrowserWindow
 
+let mainWindow
+
 app.on('ready', _ => {
-  new BrowserWindows({
+  mainWindow = new BrowserWindows({
     height: 400,
     width: 400
+  })
+
+  mainWindow.on('closed', _ => {
+    console.log('my-app closed!')
+    mainWindow = null
   })
 })
